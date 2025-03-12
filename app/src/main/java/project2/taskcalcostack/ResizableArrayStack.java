e73ab29b6872da2ad53c601768ef64637ceddcdb
Jaypb101 <Jaydenbriones45@gmail.com>
2025-03-11 21:04:58 -0700

    Added Javadoc to ResizableArrayStack

--- app/src/main/java/project2/taskcalcostack/ResizableArrayStack.java
+++ app/src/main/java/project2/taskcalcostack/ResizableArrayStack.java
@@ -11,10 +11,17 @@ public final class ResizableArrayStack<T> implements StackInterface<T> {
     private boolean integrityOK = false;
     private static final int MAX_CAPACITY = 10000;
 
+    /**
+     * Creates an empty stack of 50
+     */
     public ResizableArrayStack(){
         this(DEFAULT_SIZE);
     } // end default constructor
 
+    /**
+     * Creates a stack with a set capacity
+     * @param initialCapacity is the set capacity
+     */
     public ResizableArrayStack(int initialCapacity){
         integrityOK = false;
         checkCapacity(initialCapacity);
@@ -25,6 +32,11 @@ public final class ResizableArrayStack<T> implements StackInterface<T> {
         integrityOK = true;
     } // end constructor
 
+    /**
+     * Adds a new entry to the top of the stack
+     * @param newEntry is the added entry
+     */
+    @Override
     public void push(T newEntry){
         checkIntegrity();
         ensureCapacity();
@@ -32,6 +44,9 @@ public final class ResizableArrayStack<T> implements StackInterface<T> {
         topIndex++;
     }//end push
 
+    /**
+     * Doubles the length of the stack if full
+     */
     private void ensureCapacity(){
         if(topIndex >= stack.length -1){
             int newLength = 2 * stack.length;
@@ -40,6 +55,11 @@ public final class ResizableArrayStack<T> implements StackInterface<T> {
         }//end if
     }//end ensureCapacity
 
+    /**
+     * Removes the top entry from the stack
+     * @return the new top entry from the stack
+     */
+    @Override
     public T pop(){
         checkIntegrity();
         if(isEmpty()){
@@ -52,6 +72,10 @@ public final class ResizableArrayStack<T> implements StackInterface<T> {
         }//end if
     }//end pop
 
+    /**
+     * @return the top entry of the stack
+     */
+    @Override
     public T peek(){
         checkIntegrity();
         if(isEmpty()){
@@ -61,14 +85,25 @@ public final class ResizableArrayStack<T> implements StackInterface<T> {
         }//end if
     }//end peek
 
+    /**
+     * @return if the stack is empty
+     */
+    @Override
     public boolean isEmpty(){
         return topIndex < 0;
     }//end isEmpty
 
+    /**
+     * @return if integrityOK
+     */
     public boolean checkIntegrity(){
         return integrityOK;
     }//end checkIntegrity
 
+    /**
+     * Clears stack
+     */
+    @Override
     public void clear(){
         checkIntegrity();
         while(topIndex > -1){
@@ -77,6 +112,10 @@ public final class ResizableArrayStack<T> implements StackInterface<T> {
         }//end while
     }//end clear
 
+    /**
+     * Checks if the set capacity is too large
+     * @param capacity is the set capacity
+     */
     public void checkCapacity(int capacity){
         if(capacity > MAX_CAPACITY){
             throw new IllegalStateException("Attempt to create a stack " +
